3faaa16ebcb0c9a2d61209536b8c62f12bece557
Phil Branch <philbranch243@gmail.com>
2022-08-06 14:54:07 -0500

    all category routes done and testin well

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,10 +59,41 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  }).then(dbData => {
+    if(!dbData){
+      res.status(404).json({message: 'No category with that id found'});
+      return;
+    }
+    res.json(dbData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbData =>{
+    if(!dbData){
+      res.status(404).json({message: 'Category not found'});
+      return;
+    }
+    res.json(dbData);
+  })
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
