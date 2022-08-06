c9328d92543b82e968b48e4cb589547721cd80c9
Phil Branch <philbranch243@gmail.com>
2022-08-06 16:14:16 -0500

    all product routes finished

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -121,6 +121,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbData =>{
+    if(!dbData){
+      res.status(404).json({message: 'Product not found'});
+      return;
+    }
+    res.json(dbData);
+  })
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
